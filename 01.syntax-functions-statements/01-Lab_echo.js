d33c9660cf3fa93d7038555e1750c3d0a9701d18
Maria Kalinina <marykalinina86449@gmail.com>
2026-09-10 21:19:00 +0300

    Create 01-Lab_echo.js

---
+++ 01.syntax-functions-statements/01-Lab_echo.js
@@ -0,0 +1,10 @@
+// Task: Print String Length and Content.
+// Description: Receive one string and print its length on the first line
+// and the unchanged string on the second line.
+
+function echoFunction(string) {
+    console.log(string.length);
+    console.log(string);
+}
+echoFunction('Hello, JavaScript!');
+echoFunction('strings are easy');
