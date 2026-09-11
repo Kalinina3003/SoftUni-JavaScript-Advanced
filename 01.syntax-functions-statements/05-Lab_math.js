e37f55f599209ef0414611bbcb1ee0d3ef2d4a8f
Maria Kalinina <marykalinina86449@gmail.com>
2026-09-11 13:05:33 +0300

    Add files via upload

---
+++ 01.syntax-functions-statements/05-Lab_math.js
@@ -0,0 +1,28 @@
+// Task: Calculator.
+// Description: Receive two numbers and an operator, 
+// perform the specified mathematical operation, and print the result.
+
+function mathOperations(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            console.log(num1 + num2);
+            break;
+        case '-':
+            console.log(num1 - num2);
+            break;
+        case '*':
+            console.log(num1 * num2);
+            break;
+        case '/':
+            console.log(num1 / num2);
+            break;
+        case '%':
+            console.log(num1 % num2);
+            break;
+        case '**':
+            console.log(num1 ** num2);
+            break;
+    }
+}
+mathOperations(5, 6, '+');
+mathOperations(3, 5.5, '*');
