8173cd26aaa79c2cab619a9725197296761fbf3f
Maria Kalinina <marykalinina86449@gmail.com>
2026-09-15 15:01:12 +0300

    Add files via upload

---
+++ 01.syntax-functions-statements/04-Ex_previous.js
@@ -0,0 +1,12 @@
+// Task: Previous Day.
+// Description: Receive a year, month, and day 
+// and calculate the date of the previous day.
+
+function previousDay(year, month, day) {
+    let date = new Date(year, month - 1, day);
+    date.setDate(date.getDate() - 1);
+
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+}
+console.log(previousDay(2016, 9, 30));
+console.log(previousDay(2015, 5, 10));
