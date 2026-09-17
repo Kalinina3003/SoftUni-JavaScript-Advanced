429ddd74530faf0033a9cfdb7fb770a1d3e32040
Maria Kalinina <marykalinina86449@gmail.com>
2026-09-17 20:06:36 +0300

    Add files via upload

---
+++ 02.arrays-&-nested-arrays/05-Lab_smallest.js
@@ -0,0 +1,10 @@
+// Task: Smallest Two Numbers.
+// Description: Find and print the two smallest numbers in an array.
+
+function smallestTwoNumbers(arr) {
+    arr.sort((a, b) => a - b);
+
+    console.log(arr[0], arr[1]);
+}
+smallestTwoNumbers([30, 15, 50, 5]);
+smallestTwoNumbers([3, 0, 10, 4, 7, 3]);
