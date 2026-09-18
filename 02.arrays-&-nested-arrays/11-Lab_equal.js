8d025e0ce03522f2c53144e1a4dafebfbb9fa760
Maria Kalinina <marykalinina86449@gmail.com>
2026-09-18 15:10:08 +0300

    Add files via upload

---
+++ 02.arrays-&-nested-arrays/11-Lab_equal.js
@@ -0,0 +1,35 @@
+// Task: Equal Neighbors.
+// Description: Count and return the number 
+// of equal neighboring elements in a matrix.
+
+function equalNeighbors(matrix) {
+    let count = 0;
+
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+
+            if (col < matrix[row].length - 1 &&
+                matrix[row][col] === matrix[row][col + 1]) {
+                count++;
+            }
+
+            if (row < matrix.length - 1 &&
+                matrix[row][col] === matrix[row + 1][col]) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+console.log(equalNeighbors(
+    [['2', '3', '4', '7', '0'],
+    ['4', '0', '5', '3', '4'],
+    ['2', '3', '5', '4', '2'],
+    ['9', '8', '7', '5', '4']]
+));                                 // 1
+console.log(equalNeighbors(
+    [['test', 'yes', 'yo', 'ho'],
+    ['well', 'done', 'yo', '6'],
+    ['not', 'done', 'yet', '5']]
+));                                // 2
