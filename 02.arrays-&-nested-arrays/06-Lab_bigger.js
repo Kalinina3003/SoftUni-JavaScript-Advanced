67b8a795eb44da88adb0906e902739aa84975f88
Maria Kalinina <marykalinina86449@gmail.com>
2026-09-18 09:12:26 +0300

    Add files via upload

---
+++ 02.arrays-&-nested-arrays/06-Lab_bigger.js
@@ -0,0 +1,12 @@
+// Task: Bigger Half.
+// Description: Sort the array in ascending order 
+// and return the bigger half.
+
+function biggerHalf(arr) {
+    arr.sort((a, b) => a - b);
+    let startIndex = Math.floor(arr.length / 2);
+
+    return arr.slice(startIndex);
+}
+console.log(biggerHalf([4, 7, 2, 5]));
+console.log(biggerHalf([3, 19, 14, 7, 2, 19, 6]));
