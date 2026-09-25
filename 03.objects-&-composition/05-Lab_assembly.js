8ab26798e08d6ea4bdd487ceb1d59cb256ac90a9
Maria Kalinina <marykalinina86449@gmail.com>
2026-09-25 13:50:08 +0300

    Add files via upload

---
+++ 03.objects-&-composition/05-Lab_assembly.js
@@ -0,0 +1,90 @@
+// Task: Object Composition with Decorators.
+// Description: Create a library of decorators that add
+// different functionality to a car object.
+
+function createAssemblyLine() {
+    function hasClima(car) {
+        car.temp = 21;
+        car.tempSettings = 21;
+
+        car.adjustTemp = function () {
+            if (car.temp < car.rempSettings) {
+                car.temp++;
+            } else if (car.temp > car.tempSettings) {
+                car.temp--;
+            }
+        };
+    }
+
+    function hasAudio(car) {
+        car.currentTrack = null;
+
+        car.nowPlaying = function () {
+            if (car.currentTrack !== null) {
+                console.log(`Now playing '${car.currentTrack.name}' by ${car.currentTrack.artist}`);
+            }
+        };
+    }
+
+    function hasParktronic(car) {
+        car.checkDistance = function (distance) {
+            if (distance < 0.1) {
+                console.log('Beep! Beep! Beep!');
+            } else if (distance < 0.25) {
+                console.log('Beep! Beep!');
+            } else if (distance < 0.5) {
+                console.log('Beep!');;
+            } else {
+                console.log('');
+            }
+        };
+    }
+
+    return {
+        hasClima,
+        hasAudio,
+        hasParktronic
+    };
+}
+const assemblyLine = createAssemblyLine();
+
+const myCar = {
+    make: 'Toyota',
+    model: 'Avensis'
+};
+
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
+// 21 20; (\n)
+
+assemblyLine.hasAudio(myCar);
+myCar.currentTrack = {
+    name: 'Never Gonna Give You Up',
+    artist: 'Rick Astley'
+};
+myCar.nowPlaying();
+// Now playing 'Never Gonna Give You Up' by Rick Astley;
+
+assemblyLine.hasParktronic(myCar);
+myCar.checkDistance(0.4);
+myCar.checkDistance(0.2);
+// Beep! 
+// Beep! Beep!;
+
+console.log(myCar);
+// {
+//   make: 'Toyota',
+//   model: 'Avensis',
+//   temp: 20,
+//   tempSettings: 18,
+//   adjustTemp: [Function],
+//   currentTrack: {
+//     name: 'Never Gonna Give You Up',
+//     artist: 'Rick Astley'
+//   },
+//   nowPlaying: [Function],
+//   checkDistance: [Function]
+// }
